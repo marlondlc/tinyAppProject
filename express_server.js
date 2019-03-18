a3007ef9d408c0ed5ee44ae39f556cbf8db52400
Marlon <m.de.la.cruz@hotmail.com>
2019-03-18 04:30:53 +0000

    clean up notes/code

--- express_server.js
+++ express_server.js
@@ -1,38 +1,31 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080;
 const bodyParser = require("body-parser");
-const uuidv4 = require("uuid/v4");
-//const cookieParser = require("cookie-parser");
 const cookieSession = require("cookie-session");
 const bcrypt = require("bcrypt");
 
 app.use(cookieSession({ name: "session", keys: ["Key1", "Key2"] }));
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-//app.use(cookieParser());
 
 var urlDatabase = {
-  b2xVn2: {
-    //check if it should be strings "" when i save it
-    //shortUrl: "b2xVn2",
+  "b2xVn2": {
     longUrl: "http://www.lighthouselabs.ca",
     userId: "user3RandomID"
   },
-  u2kd8c: {
-    //shortUrl: "u2kd8c",
+  "u2kd8c": {
     longUrl: "http://www.msn.com",
     userId: "user3RandomID"
   },
   "9sm5xK": {
-    //shortUrl: "9sm5xK",
     longUrl: "http://www.google.com",
-    userId: "userRandomID" //urldb[shorturl].userId
+    userId: "userRandomID"
   }
 };
 
 const usersDb = {
+
   userRandomID: {
     id: "userRandomID",
     email: "user@example.com",
@@ -46,8 +39,9 @@ const usersDb = {
   user3RandomID: {
     id: "user3RandomID",
     email: "test@test.com",
-    password: "test"
+    password: bcrypt.hashSync("test", 10)
   }
+
 };
 
 function generateRandomString() {
@@ -57,7 +51,7 @@ function generateRandomString() {
 }
 
 const createUser = (email, password) => {
-  const user_id = uuidv4();
+  const user_id = generateRandomString();
 
   const newUser = {
     id: user_id,
@@ -70,16 +64,15 @@ const createUser = (email, password) => {
 };
 
 const getUserIdByEmail = email => {
-  //email(parameter) is coming from req.body.email
   for (const userId in usersDb) {
     if (usersDb[userId].email === email) {
-      return userId; //same as saying true
+      return userId;
     }
   }
   return false;
 };
 
-function addNewUrl(shortUrl, longUrl, userId) {
+function createNewUrlObjInDB(shortUrl, longUrl, userId) {
   urlDatabase[shortUrl] = {
     longUrl: longUrl,
     userId: userId
@@ -93,43 +86,29 @@ function urlsForUser(userId) {
       filteredUrls[urlKey] = urlDatabase[urlKey];
     }
   }
-  //console.log("this is the filtered : ", filteredUrls);
   return filteredUrls;
 }
 
 app.post("/login", (req, res) => {
   const email = req.body.email;
-  const passwordForm = req.body.password;
-  const emailPasswordEmpty = !email || !passwordForm;
+  const password = req.body.password;
+  const emailPasswordEmpty = !email || !password;
   const userId = getUserIdByEmail(email);
-  const hashedPassword = bcrypt.hashSync(req.body.password, 10);
-  const compSync = bcrypt.compareSync(passwordForm, hashedPassword); // ask about this
 
   if (emailPasswordEmpty) {
     res.status(403).send("Please fill out the required feild");
   } else if (!userId) {
     res.status(403).send("Wrong User Info!");
-  } else if (usersDb[userId].password === passwordForm) {
-    req.session["user_id"] = userId;
-    res.redirect("/urls");
   } else {
-    res.status(400).send("wrong credentials!");
+    const hashedPassword = usersDb[userId]["password"]
+    const compSync = bcrypt.compareSync(password, hashedPassword);
+    if (compSync) {
+      req.session["user_id"] = userId;
+      res.redirect("/urls");
+    } else {
+      res.status(400).send("wrong credentials!");
+    }
   }
-});
-
-app.post("/logout", (req, res) => {
-  req.session = null;
-  res.clearCookie("user_id"); // is this needed?
-  res.redirect("/urls");
-});
-
-app.post("/urls", (req, res) => {
-  const userId = req.session["user_id"];
-  const longUrl = req.body.longURL;
-  const shortUrl = generateRandomString();
-  //urlDatabase[shortUrl] = { longUrl: longUrl, userId: userId }; // function below is doing the same
-  addNewUrl(shortUrl, longUrl, userId);
-  res.redirect("/urls");
 });
 
 app.post("/register", (req, res) => {
@@ -142,21 +121,36 @@ app.post("/register", (req, res) => {
   } else if (getUserIdByEmail(email)) {
     res.status(400).send("Account already exist. Please Login!");
   } else {
-    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+    const hashedPassword = bcrypt.hashSync(password, 10);
     const userId = createUser(email, hashedPassword);
-    req.session["user_id"] = userId; // this needs to be changed to req.session.user_id = userId
+    req.session["user_id"] = userId;
     res.redirect("/urls");
   }
 });
 
+app.post("/logout", (req, res) => {
+  req.session = null;
+  res.redirect("/urls");
+});
+
+app.post("/urls", (req, res) => {
+  const userId = req.session["user_id"];
+  const longUrl = req.body.longURL;
+  const shortUrl = generateRandomString();
+
+  createNewUrlObjInDB(shortUrl, longUrl, userId);
+  res.redirect("/urls");
+});
+
+
 app.get("/urls", (req, res) => {
   const userId = req.session.user_id;
   let templateVars = { urls: urlsForUser(userId), loggedUser: usersDb[userId] };
-  //   if (!userId) {
-  //     res.redirect("/login");
-  //   } else {
-  res.render("urls_index", templateVars);
-  //}
+  if (!userId) {
+    res.redirect("/login");
+  } else {
+    res.render("urls_index", templateVars);
+  }
 });
 
 app.get("/urls/new", (req, res) => {
@@ -186,7 +180,7 @@ app.get("/urls/:shortURL", (req, res) => {
     user: usersDb[req.session.user_id],
     loggedUser: usersDb[req.session.user_id]
   };
-  res.render("urls_show", templateVars); //loggedUser: req.session.user_id put within template var
+  res.render("urls_show", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
@@ -202,20 +196,25 @@ app.get("/users.json", (req, res) => {
   res.json(usersDb);
 });
 
-app.post("/urls/:shortURL", (req, res) => {
-  urlDatabase[req.params.shortURL] = req.body.longURL;
+app.post("/urls/:shortUrl", (req, res) => {
+  urlDatabase[req.params.shortUrl] = req.body.longURL;
   res.redirect("/urls");
+
   if (req.session["user_id"] === urlDatabase[shortUrl].userId) {
     urlDatabase[shortUrl].longUrl = longUrl;
   }
 });
 
-// app.post("/urls/:shortUrl", (req, res) => {
-//   const shortUrl = req.params.shortUrl;
-//   const longUrl = req.body.longURL;
 
-//   res.redirect("/urls");
-// });
+app.post("/urls/:shortUrl/update", (req, res) => {
+  const shortUrl = req.params.shortUrl;
+  const longUrl = req.body.longURL;
+
+  if (req.session["user_id"] === urlDatabase[shortUrl].userId) {
+    urlDatabase[shortUrl].longUrl = longUrl;
+  }
+  res.redirect("/urls");
+});
 
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
@@ -225,88 +224,3 @@ app.post("/urls/:id/delete", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
-/*
-NOTES:
-
-
-//functions to create:
-
-//creates a new user and adds it of userDB
-//return the user id so we can set it in the cookies
-const createUSer = ( name, email, password) => {
-
-//generate a user ID:
-const user_id = object.key('user').length + 1;
-
-//create a new user object:
-const newUSer = {
-    id: userId,
-    name: name;
-    email: email,
-    password: password
-};
-
-//addd the user object to userDB:
-userDB[userId] = newUser;
-
-
-}
-
-
-1- app.get('/register', (req,res) => {
-
-res.render('register' , { loggedUser: null });
-})
-
-//this will create a new user in the 
-2- app.post('/register', (req,res) => {
-const name = req.body.name;
-const eamil = req.body.name;
-const password = req.body.password;
-
-//es6 version =
-const {name,email,password} = req.body
-
-// createUSer in the global scope (function above)
-const userId = createUser(name,email,password);
-
-//set the cookie with the user_id
-res.cookie(user_id,userId) // this needs to be changed to req.session.user_id = userId
-
-//once the above is done, redirect me to: (hompage)
-res.redirect('/quotes');
-})
-
-3- app.get ('login, (req,res) = {
-var templeteVars = .....
-res.render('login', templeteVar)
-
-})
-
-4- app.post('/login',(req,res) => {
-
-//extract the login info from the form
-
-//authenticate the user
-//create a function authenticate that will return false or the user id
-//check if a user with that email and password exist in th userDB
-//if userId is truthy, set the cookie and redirect
-//if userId is falsy, send error messaage
-})
-
-
-
-1 - created a register route
-GET: display the register form
-POST:
-    -extract the user info from the form with req.body
-    -create a user in the DB with those info
-    -set the user Id in the cookies
-    redirect to another page
-
-2 - pass the username in templeteVars
-    -For each EJS that uses the variable called username"loggedUser for me"
-    -the username must be passed in the TempleteVar of all the endpoints.
-
-*/
